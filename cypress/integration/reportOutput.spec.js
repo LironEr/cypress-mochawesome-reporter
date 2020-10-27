f44ebe1828104aff18d0fbb803154736d3f970cf
Liron Erman <lironerm@gmail.com>
2020-10-27 20:27:38 +0200

    chore (tests): Add tests

---
+++ cypress/integration/reportOutput.spec.js
@@ -0,0 +1,31 @@
+describe('Report output', () => {
+  ['simple', 'multiple-reporters'].forEach((folder) => {
+    describe(`${folder} folder`, () => {
+      beforeEach(() => {
+        cy.visit(`examples/${folder}/cypress/reports/html/mochawesome.html`);
+      });
+
+      it('Validate all tests exists', () => {
+        cy.get('li[title="Suites"]').contains(4);
+        cy.get('li[title="Tests"]').contains(6);
+        cy.get('li[title="Passed"]').contains(2);
+        cy.get('li[title="Failed"]').contains(3);
+        cy.get('li[title="Skipped"]').contains(1);
+      });
+
+      describe('Validate image exists', () => {
+        it('fail during test', () => {
+          cy.validateTestHasScreenshot('fail test');
+        });
+
+        it('before hook', () => {
+          cy.validateTestHasScreenshot('before hook fail');
+        });
+
+        it('beforeEach', () => {
+          cy.validateTestHasScreenshot('beforeEach hook fail 1');
+        });
+      });
+    });
+  });
+});
--- examples/multiple-reporters/cypress/integration/sub-dir/test1.spec.js
+++ examples/multiple-reporters/cypress/integration/sub-dir/test1.spec.js
@@ -1,13 +1,13 @@
-describe("Test 1", () => {
-  it("default todos exists", () => {
-    cy.visit("/");
+describe('Test 1', () => {
+  it('default todos exists', () => {
+    cy.visit('/');
 
-    cy.get("#todo-list li").should("have.length", 4);
+    cy.get('#todo-list li').should('have.length', 4);
   });
 
-  it("fail test", () => {
-    cy.visit("/");
+  it('fail test', () => {
+    cy.visit('/');
 
-    cy.get("#todo-list li").should("have.length", 10);
+    cy.get('#todo-list li').should('have.length', 10);
   });
 });
