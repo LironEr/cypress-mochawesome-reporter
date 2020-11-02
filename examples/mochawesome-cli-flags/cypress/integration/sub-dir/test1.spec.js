831efee0280b35343d75178c2f9f7b50abbea1ab
LironEr <lironerm@gmail.com>
2020-11-02 19:42:49 +0200

    feat: capture every screenshot (for failed & passed tests) (#12)

--- examples/mochawesome-cli-flags/cypress/integration/sub-dir/test1.spec.js
+++ examples/mochawesome-cli-flags/cypress/integration/sub-dir/test1.spec.js
@@ -3,10 +3,14 @@ describe('Test 1', () => {
     cy.visit('/');
 
     cy.get('#todo-list li').should('have.length', 4);
+
+    cy.screenshot();
   });
 
   it('fail test', () => {
     cy.visit('/');
+
+    cy.screenshot('custom-name');
 
     cy.get('#todo-list li').should('have.length', 10);
   });
