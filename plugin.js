ec926f057765255c362694c3d26875c06d487bba
LironEr <lironerm@gmail.com>
2021-03-28 15:30:37 +0300

    chore: release v2.0.0 (#23)
    * feat: Get cypress config & generate the report using cypress events (#21)
    
    * chore (deps): remove mocha dependency (#22)
    
    * bump version

---
+++ plugin.js
@@ -0,0 +1,15 @@
+const fse = require('fs-extra');
+const generateReport = require('./lib/generateReport');
+const { getSimpleCypressConfig } = require('./lib/utils');
+
+module.exports = function (on) {
+  on('before:run', async (details) => {
+    const { outputDir } = getSimpleCypressConfig(details.config);
+    console.log(`Remove output folder ${outputDir}`);
+
+    await fse.remove(outputDir);
+  }),
+  on('after:run', async (results) => {
+    await generateReport(results.config);
+  })
+}
