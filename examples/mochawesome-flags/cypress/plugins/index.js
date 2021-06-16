a37ce0b13a5ba33ecaeb6ae411a15ca0ffed9fcb
LironEr <lironerm@gmail.com>
2021-06-16 19:38:53 +0300

    feat: export beforeRunHook & afterRunHook functions

--- examples/mochawesome-flags/cypress/plugins/index.js
+++ examples/mochawesome-flags/cypress/plugins/index.js
@@ -1,16 +1,13 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
+module.exports = (on) => {
+  on('before:run', async (details) => {
+    console.log('override before:run');
+    await beforeRunHook(details);
+  });
 
-module.exports = (on, config) => {
-  require('cypress-mochawesome-reporter/plugin')(on);
-}
+  on('after:run', async (results) => {
+    console.log('override after:run');
+    await afterRunHook(results);
+  });
+};
