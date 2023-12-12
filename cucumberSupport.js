b57ad3ed915a04d7463115beff6754aa1509dadb
Damien SOREL <damien.sorel@sogelink.com>
2023-12-12 16:28:55 +0100

    add support for cypress-cucumber-preprocessor

---
+++ cucumberSupport.js
@@ -0,0 +1,34 @@
+const { Before } = require('@badeball/cypress-cucumber-preprocessor');
+const consts = require('./lib/consts');
+
+/**
+ * Store the Cucumber source in the context for later usage
+ */
+Before(() => {
+    const gherkinDocument = window.testState?.gherkinDocument;
+    const pickle = window.testState?.pickle;
+
+    if (gherkinDocument && pickle) {
+        // (poor-man deep copy)
+        const gherkinDocumentWithSingleScenario = JSON.parse(JSON.stringify({
+            ...gherkinDocument,
+            feature: {
+                ...gherkinDocument.feature,
+                // keep only the scenario corresponding to the current pickle
+                children: gherkinDocument.feature.children.filter(f => f.scenario?.id === pickle.astNodeIds[0]),
+            },
+        }));
+
+        // for scenario outlines, only keep the corresponding example
+        const scenario = gherkinDocumentWithSingleScenario.feature.children[0].scenario;
+        if (scenario.examples.length) {
+            const example = scenario.examples[0];
+            example.tableBody = example.tableBody.filter(row => row.id === pickle.astNodeIds[1]);
+        }
+
+        cy.addTestContext({
+            title: consts.cucumberStepsContextKey,
+            value: gherkinDocumentWithSingleScenario,
+        });
+    }
+});
