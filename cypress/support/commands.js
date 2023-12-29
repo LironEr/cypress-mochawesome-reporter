28ed4268acc7bb207ecbd5754f8f3156d01622d3
LironEr <lironerm@gmail.com>
2023-12-29 13:51:21 +0200

    chore: add tests and improve docs for cypress-cucumber-preprocessor (#175)
    * chore: add tests and improve docs for cypress-cucumber-preprocessor
    
    * use node 18
    
    * try install with --concurrency 1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -19,32 +19,51 @@ Cypress.Commands.add('validateTestHasScreenshot', (testTitle, numberOfScreenshot
         });
     });
 });
+
 Cypress.Commands.add('validateTestHasVideo', (testTitle) => {
-    cy.get(`[title="${testTitle}"]`)
-        .click()
-        .parents('li[class*="test--component"]')
-        .then(([$el]) => {
-            cy.wrap($el)
-                .find('video')
-                .then(($vids) => {
-                    cy.wrap($vids)
-                        .should('have.length', 1)
-                        .each(([$vid]) => {
-                            cy.wrap($vid)
-                                .should('be.visible')
-                                .find('source')
-                                .should('have.attr', 'type', 'video/mp4')
-                        });
-                });
+  cy.get(`[title="${testTitle}"]`)
+    .click()
+    .parents('li[class*="test--component"]')
+    .then(([$el]) => {
+      cy.wrap($el)
+        .find('video')
+        .then(($vids) => {
+          cy.wrap($vids)
+            .should('have.length', 1)
+            .each(([$vid]) => {
+              cy.wrap($vid).should('be.visible').find('source').should('have.attr', 'type', 'video/mp4');
+            });
         });
+    });
 });
+
 Cypress.Commands.add('validateTestHasNoVideo', (testTitle) => {
-    cy.get(`[title="${testTitle}"]`)
-        .click()
-        .parents('li[class*="test--component"]')
-        .then(([$el]) => {
-            cy.wrap($el)
-                .find('video')
-                .should('not.exist')
+  cy.get(`[title="${testTitle}"]`)
+    .click()
+    .parents('li[class*="test--component"]')
+    .then(([$el]) => {
+      cy.wrap($el).find('video').should('not.exist');
+    });
+});
+
+Cypress.Commands.add('validateTestCode', (testTitle, content) => {
+  // cy.get('code').debug();
+  cy.get(`[title="${testTitle}"]`)
+    .click()
+    .parents('li[class*="test--component"]')
+    .then(([$el]) => {
+      cy.wrap($el)
+        .find('code')
+        .then(($codes) => {
+          cy.wrap($codes)
+            .last()
+            .wrap(($code) => {
+              cy.wrap($code)
+                .should('be.visible')
+                .wrap(($c) => {
+                  expect($c.text().trim(), 'match content').to.be.equal(content.trim());
+                });
+            });
         });
+    });
 });
